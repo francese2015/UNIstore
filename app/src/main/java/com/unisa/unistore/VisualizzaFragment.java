bb38f4054f135356c560086b47fb83045a1edc9e
Daniele Iannone <diannone3@gmail.com>
2015-06-21 22:53:07 +0200

    Minor bug fix.

--- app/src/main/java/com/unisa/unistore/VisualizzaFragment.java
+++ app/src/main/java/com/unisa/unistore/VisualizzaFragment.java
@@ -14,7 +14,7 @@ public class VisualizzaFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
  
-        View rootView = inflater.inflate(R.layout.fragment_visualizza, container, false);
+        View rootView = inflater.inflate(R.layout.activity_view_notice_detail, container, false);
          
         return rootView;
     }
